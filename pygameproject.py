bf3587e4fa56ea2cbd2ac0a25660ad593cf1275b
Eduardo Perez <pz.eduardo@hotmail.com>
2023-02-01 11:02:10 -0800

    Key inputs

--- pygameproject.py
+++ pygameproject.py
@@ -2,15 +2,9 @@ import pygame
 
 def main():
     pygame.init()
-    width = 100
-    height = 200
-    surface = pygame.Surface((width, height))
-
     screen = pygame.display.set_mode((800,400))
-    
     pygame.display.set_caption("GameGameGameGame")
     clock = pygame.time.Clock()
-
 
     sky_surface = pygame.image.load('graphics/sky_background.png')
     sky_surface = pygame.transform.scale(sky_surface, (800,300))
@@ -21,10 +15,10 @@ def main():
     text_font = pygame.font.Font('Font/Pixeltype.ttf', 60)
 
     text_surface = text_font.render('Penguin Jumping Game', False, 'Black')
-
+    #Penguin Sprite
     penguin_surface = pygame.image.load('graphics/penguin.png').convert_alpha()
     penguin_surface = pygame.transform.scale(penguin_surface, (100,100))
-
+    #Snake Sprite
     snake_surface = pygame.image.load("graphics/snake.png").convert()
     snake_surface = pygame.transform.scale(snake_surface, (100,100))
 
@@ -34,14 +28,37 @@ def main():
     snake_x = 400
     snake_y = 250
 
+    MOVE_UP = 220
+    MOVE_DOWN = 300
+    MOVE_MIDDLE = 250
+
     while True:
         for event in pygame.event.get():
+            #Quit the game in case of closing it
             if event.type == pygame.QUIT:
                 pygame.quit()
                 break
+            #React to the keys inputs as they are pressed
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_UP]:
+                    if penguin_y == MOVE_MIDDLE:
+                        penguin_y = MOVE_UP
+                        print(1)
+                    if penguin_y == MOVE_DOWN:
+                        penguin_y = MOVE_MIDDLE
+                        print(2)
+                #FIX KEY DOWN
+                if keys[pygame.K_DOWN]:
+                    if penguin_y == MOVE_UP:
+                        penguin_y = MOVE_MIDDLE
+                        print(3)
+                    if penguin_y == MOVE_MIDDLE:
+                        penguin_y = MOVE_DOWN
+                        print(4)
         
         screen.blit(sky_surface,(0,0))
-        screen.blit(ground_surface, (0, 300))
+        screen.blit(ground_surface, (0, 300)) 
         screen.blit(text_surface, (300, 50))
         penguin_x += 1
         snake_x -= 4
@@ -53,7 +70,7 @@ def main():
         
         pygame.display.update()
         clock.tick(60)
-
+        #52:34
 
 if __name__ == "__main__":
     main()
